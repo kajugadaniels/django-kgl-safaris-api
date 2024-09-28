380202954c31d7b40675e537262af17b946eff2d
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-09-28 18:23:56 +0200

    Add corsheader

--- api/settings.py
+++ api/settings.py
@@ -41,6 +41,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -220,3 +221,8 @@ CKEDITOR_CONFIGS = {
         'image2_disableResizer': False,
     },
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',  # Add your frontend URL here
+]
