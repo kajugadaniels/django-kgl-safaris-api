c241347f5bf36dafd77ad5391cf89069eb8746e4
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-09-28 18:09:50 +0200

    Create tour package view

--- home/views.py
+++ home/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from home.models import *
+from home.serializers import *
+from django.utils.text import slugify
+from rest_framework.views import APIView
+from rest_framework import status, generics
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+class TourPackageListCreateView(generics.ListCreateAPIView):
+    queryset = TourPackage.objects.all()
+    serializer_class = TourPackageSerializer
+
+    def perform_create(self, serializer):
+        title = serializer.validated_data.get('title')
+        slug = slugify(title)
+        serializer.save(slug=slug)
