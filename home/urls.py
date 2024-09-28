e7364b92602dc390f32c4b59897d8da43b640a98
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-09-28 18:11:22 +0200

    Tour packages route

--- home/urls.py
+++ home/urls.py
@@ -6,5 +6,6 @@ from django.conf.urls.static import static
 app_name = 'base'
 
 urlpatterns = [
-    # 
+    path('tour-packages/', TourPackageListCreateView.as_view(), name='tourpackageListCreate'),
+    path('tour-packages/<slug:slug>/', TourPackageDetailView.as_view(), name='tourpackageDetail'),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
