8d6fe310fb8d4495c2d35819ec79908148d3c2f9
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-09-28 18:10:18 +0200

    Retrieve tour package view

--- home/views.py
+++ home/views.py
@@ -14,3 +14,19 @@ class TourPackageListCreateView(generics.ListCreateAPIView):
         title = serializer.validated_data.get('title')
         slug = slugify(title)
         serializer.save(slug=slug)
+
+class TourPackageDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = TourPackage.objects.all()
+    serializer_class = TourPackageSerializer
+    lookup_field = 'slug'
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        if 'title' in request.data:
+            serializer.save(slug=slugify(request.data['title']))
+        else:
+            serializer.save()
+        return Response(serializer.data)
